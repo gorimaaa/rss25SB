852fb49948342688747bae9b3cb6a6969c9bdb51
Gorimaaa <juba.belhocine@outlook.fr>
2025-04-28 22:48:55 +0200

    Ajout de la bdd

--- src/main/java/fr/univrouen/rss25SB/Controllers/GetController.java
+++ src/main/java/fr/univrouen/rss25SB/Controllers/GetController.java
@@ -1,7 +1,9 @@
 package fr.univrouen.rss25SB.Controllers;
 
+import fr.univrouen.rss25SB.Entity.Message;
 import fr.univrouen.rss25SB.Model.Item;
 import fr.univrouen.rss25SB.Model.TestRSS;
+import fr.univrouen.rss25SB.Repository.MessageRepository;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,12 @@ import java.io.IOException;
 
 @RestController
 public class GetController {
+    private final MessageRepository messageRepository;
+
+    public GetController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
     @GetMapping("/resume")
     public String getListRSSinXML() {
         return "Envoi de la liste des flux RSS";
@@ -48,5 +56,13 @@ public class GetController {
     public @ResponseBody Item getXML() {
         Item it = new Item("12345678","Test item","2022-05-01T11:22:33");
         return it;
+    }
+
+    @GetMapping("/testdb")
+    public String testDatabase(){
+        Message message = new Message();
+        message.setMessage("Ceci est un test");
+        messageRepository.save(message);
+        return "Tout s'est bien passé!";
     }
 }
