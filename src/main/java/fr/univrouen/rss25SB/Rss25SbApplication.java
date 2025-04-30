a697d023b6518ed9189a3a5e46d5cf6fb0a3808a
Zabili <zakaria782001@outlook.fr>
2025-04-30 09:55:20 +0200

    premier commit

--- src/main/java/fr/univrouen/rss25SB/Rss25SbApplication.java
+++ src/main/java/fr/univrouen/rss25SB/Rss25SbApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Rss25SbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Rss25SbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Rss25SbApplication.class, args);
+    }
 
 }
